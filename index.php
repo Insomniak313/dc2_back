0c510dac35cdf7155b7ac272ab34682619f516a3
Mehdi Tarhoult <mehdi.trh@outlook.fr>
2020-12-06 15:11:44 +0100

    New feature : we can log out!
    Added 2 functions in the "if" statement, and another one to log out
    Readability of index.php and UX improved by adding placeholders in the two input fields

--- index.php
+++ index.php
@@ -1,7 +1,20 @@
 <?php
-
 session_start();
 
+function isAlreadyConnected() {
+    return isset($_SESSION['est_connecte']) AND $_SESSION['est_connecte'];
+}
+
+function isFormCompleated() {
+    return isset($_POST['username']) AND isset($_POST['password']);
+}
+
+function logOutUser() {
+    $_POST = array();
+    $_SESSION = array();
+    session_destroy();
+    header('Refresh:0');
+}
 ?>
 
 <!DOCTYPE html>
@@ -13,17 +26,24 @@ session_start();
 
 <?php
 
-
-if(isset($_POST['username']) && isset($_POST['password']) || isset($_SESSION['est_connecte']) && $_SESSION['est_connecte'] === true) {
-    echo 'Vous êtes connecté';
+// Checking if we clicked 'Se déconnecter'
+if (isset($_POST['log_out'])) {
+    logOutUser();
+} elseif (isFormCompleated() OR isAlreadyConnected()) {
     $_SESSION['est_connecte'] = true; // Flag si je suis connecté
-}
-else {
+    ?>
+    <p>Vous êtes connecté.</p>
+    <form action="./index.php" method="POST">
+        <input type="hidden" name="log_out" />
+        <input type="submit" value="Se déconnecter" />
+    </form>
+    <?php
+} else {
     ?>
     <form action="./index.php" method="POST">
-        <input type="text" name="username" />
-        <input type="password" name="password" />
-        <input type="submit" />
+        <input type="text" name="username" placeholder="Nom d'utilisateur" />
+        <input type="password" name="password" placeholder="Mot de passe" />
+        <input type="submit" value="Se connecter" />
     </form>
     <?php
 }
