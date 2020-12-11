d6b93618258d94de58f3d45c6b126d6617ece8e9
Adrien Fournié <adrien.fournie@doyoogo.com>
2020-12-11 18:42:54 +0100

    Cours SQL

--- index.php
+++ index.php
@@ -1,107 +1,124 @@
 <?php
 
 
-/**
- * Class Application
- * Classe permettant de récupérer le paramètre "p" dans l'URL et
- * d'aiguiller vers la bonne application
- */
-class Application {
+/*
 
-    /**
-     * Fonction principale de notre application
-     */
-    public function lancer() {
-        $routeur = new Routeur();
-        $routeur->trouveRoute($_GET['p']);
-    }
-}
 
-/**
- * Class Routeur
- * Classe permettant d'afficher la bonne page (texte affiché par une fonction d'une classe)
- * en fonction d'une url
- */
-class Routeur {
+utilisateurs
+-------------
+id
+pseudo
+password
 
-    /**
-     * Fonction permettant d'afficher la bonne page en fonction d'une URL
-     * @param $url
-     */
-    public function trouveRoute($url) {
-        // En fonction de l'URL on va récupérer le contenu dans un controleur ou un autre
-        switch ($url) {
-            case 'home':
-                $controleur = new HomeControleur();
-                $contenu = $controleur->index();
-                break;
 
-            case 'about':
-                $controleur = new AboutControleur();
-                $contenu = $controleur->index();
-                break;
 
-            default:
-                $controleur = new ErrorControleur();
-                $contenu = $controleur->index();
-                break;
-        }
+# INSERER DES VALEURS
 
-        // Ensuite on affiche (echo) la page avec son contenu
-        $rootControleur = new RootControleur();
-        $rootControleur->afficherPage($contenu);
-    }
-}
+INSERT INTO utilisateurs VALUES (12, 'L''Asterix', 'test');
 
-/**
- * Class RootControleur
- * Notre template de page principal, qui va inclure les autres
- */
-class RootControleur {
-    public function afficherPage($contenu) {
-        echo '<!DOCTYPE html>
-<html>
-<head>
-	<title></title>
-</head>
-<body>' . $contenu . '
-</body>
-</html>';
-    }
-}
+# JE PRECISE LES COLONNES
+INSERT INTO utilisateurs (pseudo, password) VALUES ('Hugo', 'pass');
 
-/**
- * Class HomeControleur
- * Page d'accueil
- */
-class HomeControleur {
-    public function index() {
-        return '<div>HOME</div>';
-    }
-}
 
-/**
- * Class AboutControleur
- * Page a propos
- */
-class AboutControleur {
-    public function index() {
-        return '<div>ABOUT</div>';
-    }
-}
+# INSERER 2 LIGNES
+INSERT INTO utilisateurs VALUES (12, 'Adrien', 'test'), (13, 'Adrien 2 ', 'test 2');
 
-/**
- * Class ErrorControleur
- * Page d'erreur
- */
-class ErrorControleur {
-    public function index() {
-        return '<div>ERROR</div>';
-    }
-}
 
-// On instancie l'application (on crée un nouvel objet de type Application)
-$app = new Application();
-// Et on lance l'app
-$app->lancer();
+# LIRE LES LIGNES
 
+SELECT id, pseudo FROM utilisateurs WHERE id = 4;
+
+SELECT id, pseudo, password FROM utilisateurs WHERE id = 4;
+
+
+CONDITIONS WHERE
+
+OPERATEURS :
+id = 4
+id > 4
+id >= 4
+id <  4     strict inferieur
+id <= 4      inferieur egal
+id <> 4      différent
+
+# * = toutes les colonnes
+SELECT * FROM utilisateurs; # REQUETE DE BASE
+
+SELECT id FROM utilisateurs WHERE pseudo = 'Arthur' AND password = 'test' OR pseudo = 'Josephine';
+
+
+SELECT colonnnes
+FROM ma_table
+(WHERE condition_1 AND condition_2 OR condition_3 ...)
+(ORDER BY )
+
+
+
+SELECT id FROM utilisateurs WHERE pseudo = 'Arthur' ORDER BY password ASC ;
+
+id
+54		Arthur		aze
+22		Arthur		bcd
+14		Arthur		test
+6		Arthur		vvv
+
+
+
+# Décroissante : DESC
+# Croissant : ASC
+
+14
+54
+22
+6
+
+# SUPPRIMER DES LIGNES
+
+DELETE FROM utilisateurs;
+DELETE FROM utilisateurs WHERE id = 4;
+
+# METTRE A JOUR DES LIGNES
+
+UPDATE utilisateurs SET pseudo = 'Clement' WHERE id = 12;
+UPDATE utilisateurs SET pseudo = 'Clement', password = 'RIENDUTOUT' WHERE id = 12;
+
+# COMMENT FAIRE LORSQU'ON A PLUSIEURS TABLES
+
+
+article
+------------
+id			INT
+titre		VARCHAR(100)
+contenu		TEXT
+categorie_id		INT
+
+categorie
+------------------
+id			INT
+titre		VARCHAR(50)
+couleur		VARCHAR(7)
+
+
+
+
+article
+-------------------
+1		Bienvenue		Lorem ipsum... 				2
+2		A propos		Lorem ipsum...				1
+
+
+categorie
+---------------
+1		Dernières publis		#0186ab
+2		Archives				#132456
+
+
+Récupérez le titre des articles avec la couleur de leur catégorie
+
+
+SELECT art.id AS article_id FROM article art, categorie WHERE art.categorie_id = categorie.id
+
+
+article_id
+1
+2
